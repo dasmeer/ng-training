645e6d57fdf5f9fcc50e82d5da15550b8a15261c
dasmeer <qast@bk.ru>
2019-08-06 22:51:46 +0530

    nv version 7.1.1

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
     title = 'directives';
