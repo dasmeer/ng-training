9fc756c9597dd64b61dd1ddbbc6c5507ab6b665d
Andrey Smirnov1 <Andrey_Smirnov1@epam.com>
2019-08-06 12:33:45 +0530

    ng for

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives';
-  ifValue = true;
+    title = 'directives';
+    isDay = true;
+    floors = Array(3);
+    houses = [3, 2, 3];
 
-  makeItTrue= () => this.ifValue = true;
-  makeItFalse = () => this.ifValue = false;
+    makeItDay = () => this.isDay = true;
+    makeItNight = () => this.isDay = false;
+
+    getColourClass = (houseIndex) => {
+        
+    };
+
+    isLighted = () => this.isDay ? false : Math.random() > .4;
 }
