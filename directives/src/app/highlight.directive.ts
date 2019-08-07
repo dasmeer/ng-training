636356b95a73dc201dd8f0357a06c6878e88c350
Andrey Smirnov1 <Andrey_Smirnov1@epam.com>
2019-08-07 12:05:35 +0530

    HighlightDirective

---
+++ directives/src/app/highlight.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+    selector: '[highlight]'
+})
+export class HighlightDirective {
+    constructor(private el: ElementRef) { }
+
+    @HostListener('mouseenter') onMouseEnter() {
+        this.highlight('gold');
+    }
+
+    @HostListener('mouseleave') onMouseLeave() {
+        this.highlight(null);
+    }
+
+    private highlight(color: string) {
+        this.el.nativeElement.style.backgroundColor = color;
+    }
+}
